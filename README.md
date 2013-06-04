5873e4260b09258666a83f5c8517f49852c0af74
Benedikt Kristinsson <benedikt@audkenni.is>
2013-06-04 16:56:46 +0000

    top list

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 reddit-communities
-==================
+==============
+
+This is my humble try at crawling and graphic reddit, with the purpose
+of analysing it as a social network.
+
+How?
+--------------
+
+Subreddits commonly link to other subreddits in their "sidebar". This is available with the Reddit API. I then interpret each subreddit as a vertex in the graph and the links as edges. 
+
+Novel findings
+---------------
+
+So far I have crawled about 17,000 subreddits (vertices) and just above 100,000 links (edges) between them. There are much more according to metareddit. 
+
+I have found that some communities are topologically disconnected from the rest of reddit. But since we have a directed graph, they are not nessecarily topologically disconnected from reddit. A prime exaple of this are the communities formed around /r/clojure/. 
+
+If we order subreddits with respect to their in-degree (number of subreddits linking to them) and without regard of the number of subscribers, we reveal one interesting statistic abour reddit. 
+
+This is the top 10:
+     $ python graph.py
+     0. /r/music/
+     1. /r/kateupton/
+     2. /r/emmawatson/
+     3. /r/starlets/
+     4. /r/mileycyrus/
+     5. /r/jenniferlawrence/
+     6. /r/vanessahudgens/
+     7. /r/emmastone/
+     8. /r/emiliaclarke/
+     9. /r/arianagrande/
+
+In case you are wondering, the Top 100 list shows the same behavoir. Wtf, Reddit?
+
+Author
+-----------------
+Benedikt Kristinsson 
+benedikt.k@gmail.com
--- graph.py
+++ graph.py
@@ -1,4 +1,19 @@
-import sqlite3, igraph
+import sqlite3
+import igraph
+
+def reddit():
+    """Return an igraph instance of Reddit from data/reddit.db"""
+    conn = sqlite3.connect('data/reddit.db')
+    cursor = conn.cursor()
+    # TODO: Use the dbmodel. 
+    vertices = set([a[0] for a in cursor.execute("select name, subscribers from subreddits")])
+    edges = set([(a, b) for a, b in cursor.execute("select * from mapping") if (a in vertices) and (b in vertices)])
+    
+    
+    g = igraph.Graph(directed=True)
+    g.add_vertices(list(vertices))
+    g.add_edges(list(edges))
+    return g
 
 def get_shortest_paths(graph, node, to):
     l = [[graph.vs(a)['name'] for a in path]
@@ -11,18 +26,27 @@ def print_shortest_paths(graph, node, to):
             print graph.vs[node]['name'], 
         print
 
+def top(n, g):
+    """Return the list of top N subreddits, by in-degree, where g is the
+    graph over Reddit. In some sense, these can be viewed as the most
+    popular subreddits.
+
+    This revealed some (mildly) interesting facts about Reddit.
+
+    The default for this function is to take all subreddits into
+    consideration. If we start filtering based on number of
+    subscribers, we see different results.
+
+    """
+    degrees = [(i['name'], g.degree(i, type="in")) for i in g.vs]
+    top = sorted(degrees, key=lambda a: a[1], reverse=True)[0:n]
+    return top
 
 if __name__ == "__main__":
-    conn = sqlite3.connect('data/reddit.db')
-    cursor = conn.cursor()
-    vertices = set([a[0] for a in cursor.execute("select name from subreddits where subscribers >= 100000;")])
-    edges = set([(a, b) for a, b in cursor.execute("select * from mapping") if (a in vertices) and (b in vertices)])
-    
-    
-    g = igraph.Graph(directed=True)
-    g.add_vertices(list(vertices))
-    g.add_edges(list(edges))
-#glayout = g.layout("fr")
+    g = reddit()
+    for i, subreddit in enumerate(top(100, g)):
+        print i, ":", subreddit[0]
+    #glayout = g.layout("fr")
 
 # Top 10
 #  >>> sorted([(i['name'], g.degree(i, type="in")) for i in g.vs], key=lambda a: a[1], reverse=True)[0:10]
